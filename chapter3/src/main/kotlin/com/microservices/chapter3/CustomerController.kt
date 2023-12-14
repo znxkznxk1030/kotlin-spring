5f6b9f25d9395e1f6795ad0a42f0e8d802640c79
arthur kim <znxkznxk1030@gmail.com>
2023-12-15 00:24:39 +0900

    Restful HTTP

--- chapter3/src/main/kotlin/com/microservices/chapter3/CustomerController.kt
+++ chapter3/src/main/kotlin/com/microservices/chapter3/CustomerController.kt
@@ -1,11 +1,7 @@
 package com.microservices.chapter3
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.concurrent.ConcurrentHashMap
 
 @RestController
@@ -19,4 +15,21 @@ class CustomerController {
     @RequestMapping(value = ["/customers"], method = [RequestMethod.GET])
     fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String) =
         customers.filter { it.value.name.contains(nameFilter, true) }.values.toList()
+
+
+    @RequestMapping(value = ["/customer"], method = [RequestMethod.POST])
+    fun createCustomer(@RequestBody customer: Customer) {
+        customers[customer.id] = customer
+    }
+
+    @RequestMapping(value = ["/customer/{id}"], method = [RequestMethod.DELETE])
+    fun deleteCustomer(@PathVariable id: Int) {
+        customers.remove(id)
+    }
+
+    @RequestMapping(value = ["/customer/{id}"], method = [RequestMethod.PUT])
+    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer) {
+        customers.remove(id)
+        customers[customer.id] = customer
+    }
 }
