2c43e36307d474f0b38938481eb49c8cbdbdfdbc
arthur kim <znxkznxk1030@gmail.com>
2024-01-01 02:55:43 +0900

    Repository 를 사용 하기 때문에 컬렉션을 만들 필요가 없음.

--- chapter4/src/main/kotlin/com/microservices/chapter4/DatabaseInitializer.kt
+++ chapter4/src/main/kotlin/com/microservices/chapter4/DatabaseInitializer.kt
@@ -2,37 +2,29 @@ package com.microservices.chapter4
 
 import com.microservices.chapter4.repository.CustomerRepository
 import com.microservices.chapter4.vo.Customer
+import com.microservices.chapter4.vo.Telephone
 import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.stereotype.Component
 
 @Component
 class DatabaseInitializer {
-    @Autowired
-    lateinit var mongoOperations: ReactiveMongoOperations
 
     @Autowired
     lateinit var customerRepository: CustomerRepository
 
+    companion object {
+        val initialCustomers = listOf(
+                Customer(1, "Kotlin"),
+                Customer(2, "Spring"),
+                Customer(3, "Microservice", Telephone("+44", "7123456789"))
+        )
+    }
+
     @PostConstruct
     fun initData() {
-        mongoOperations
-                .collectionExists("Customers")
-                .subscribe {
-                    if (it != true) {
-                        mongoOperations
-                                .createCollection("Customers")
-                                .subscribe {
-                                    println("Customers collections created")
-                                }
-                    } else {
-                        println("Customers collections already exist")
-                    }
-
-                    customerRepository.save(Customer(1, "spring")).subscribe{
-                        println("Default customers created")
-                    }
-                }
+        customerRepository.saveAll(initialCustomers).subscribe{
+            println("Default customers created")
+        }
     }
 }
