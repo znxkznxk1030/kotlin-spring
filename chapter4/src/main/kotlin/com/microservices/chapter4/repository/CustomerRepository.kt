c88a2fe0242ddd9d35d1ed1c38c6dd79631e967b
arthur kim <znxkznxk1030@gmail.com>
2024-01-02 00:14:45 +0900

    ReactiveMongoTemplate 사용

--- chapter4/src/main/kotlin/com/microservices/chapter4/repository/CustomerRepository.kt
+++ chapter4/src/main/kotlin/com/microservices/chapter4/repository/CustomerRepository.kt
@@ -1,7 +1,29 @@
 package com.microservices.chapter4.repository
 
 import com.microservices.chapter4.vo.Customer
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import com.microservices.chapter4.vo.Telephone
+import jakarta.annotation.PostConstruct
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 
-interface CustomerRepository: ReactiveCrudRepository<Customer, Int> {
+@Repository
+class CustomerRepository(private val template: ReactiveMongoTemplate) {
+
+    companion object {
+        val initialCustomers = listOf(
+                Customer(1, "Kotlin"),
+                Customer(2, "Spring"),
+                Customer(3, "Microservice", Telephone("+44", "7123456789"))
+        )
+    }
+
+    @PostConstruct
+    fun initialRepository() = initialCustomers
+            .map { it.toMono() }
+            .map(this::create)
+            .map(Mono<Customer>::subscribe)
+
+    fun create(customer: Mono<Customer>) = template.save(customer)
 }
