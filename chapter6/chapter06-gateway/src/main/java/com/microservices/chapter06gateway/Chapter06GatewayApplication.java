cd41ae5555cb7703209a0def023218992e57266d
arthur kim <znxkznxk1030@gmail.com>
2024-01-28 23:53:48 +0900

    Gateway 서버 설정 #1

---
+++ chapter6/chapter06-gateway/src/main/java/com/microservices/chapter06gateway/Chapter06GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.chapter06gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter06GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter06GatewayApplication.class, args);
+	}
+
+}
