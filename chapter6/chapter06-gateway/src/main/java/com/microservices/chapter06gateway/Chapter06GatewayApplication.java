76861ff80029310de367c5ec4fd128fa2a1692fb
arthur kim <znxkznxk1030@gmail.com>
2024-02-03 19:02:25 +0900

    Spring Cloud Gateway #1

--- chapter6/chapter06-gateway/src/main/java/com/microservices/chapter06gateway/Chapter06GatewayApplication.java
+++ chapter6/chapter06-gateway/src/main/java/com/microservices/chapter06gateway/Chapter06GatewayApplication.java
@@ -2,10 +2,8 @@ package com.microservices.chapter06gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 public class Chapter06GatewayApplication {
 
 	public static void main(String[] args) {
