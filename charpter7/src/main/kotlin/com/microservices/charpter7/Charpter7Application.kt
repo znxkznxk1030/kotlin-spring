61397dfb33a60a057f7aab342ad7b772f5aeb484
arthur kim <znxkznxk1030@gmail.com>
2024-02-11 23:58:50 +0900

    Chapter 7 도커 연동

---
+++ charpter7/src/main/kotlin/com/microservices/charpter7/Charpter7Application.kt
@@ -0,0 +1,11 @@
+package com.microservices.charpter7
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Charpter7Application
+
+fun main(args: Array<String>) {
+	runApplication<Charpter7Application>(*args)
+}
---
+++ charpter7/src/test/kotlin/com/microservices/charpter7/Charpter7ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservices.charpter7
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Charpter7ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
