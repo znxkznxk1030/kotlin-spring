8e2912950803e545ac34934c400ca8c21ace3e50
arthur kim <znxkznxk1030@gmail.com>
2024-02-12 16:48:58 +0900

    Dockerfile 생성

--- charpter7/src/main/kotlin/com/microservices/charpter7/Charpter7Application.kt
+++ charpter7/src/main/kotlin/com/microservices/charpter7/Charpter7Application.kt
@@ -2,10 +2,18 @@ package com.microservices.charpter7
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class Charpter7Application
 
 fun main(args: Array<String>) {
 	runApplication<Charpter7Application>(*args)
+}
+
+@RestController
+class GreetingsController {
+	@GetMapping("/greetings")
+	fun greetings() = "hello from a Docker."
 }
